c17d5fb38ac8a5ad5e29fe26b244b7b303b7e4ef
paeb <brandon_pae@horacemann.org>
2021-11-10 08:49:11 -0500

    hello

--- Piece.py
+++ Piece.py
@@ -1,2 +1,14 @@
+from enum import Enum
+
+class Color(Enum):
+    black = 0
+    white = 1
+
 class Piece:
-    
+    position = None
+    color = None
+
+    def set_Color():
+
+    def set_Position():
+
