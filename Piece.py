02b5a99ca4b7457a5ba36481d51a1d31af41cf93
Vincent Li <Vincent@Cynthias-MacBook-Air.local>
2021-11-10 09:11:48 -0500

    Added a gameplay class and defined the check_piece function

--- Piece.py
+++ Piece.py
@@ -5,6 +5,8 @@ class Color(Enum):
     white = 1
 
 class Piece():
-    def __init__(self,position,color):
-        self._position = position
+    def __init__(self,x,y,color):
+        self._x = x
+        self._y = y
         self._color = color
+
