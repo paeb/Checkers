597469d42d46d2f96435a21f9ebe1ca6ce6a70cc
paeb <brandon_pae@horacemann.org>
2021-11-11 09:00:27 -0500

    updated file

--- Gameplay.py
+++ Gameplay.py
@@ -1,17 +1,20 @@
 from Piece import Piece
-
-
+from Board import Board
 
 class Gameplay():
     def __init__(self, pieces):
         self.pieces = pieces
 
-    def check_piece(x,y): # Check if a piece is in a given position
+    def check_piece(self,x,y): # Check if a piece is in a given position
         check = False
-        for p in pieces:
+        for p in self.pieces:
             if (p.x == x and p.y == y):
                 check = True
         return check
 
     def move(self,piece,direction):
+        pass
         # Direction is either forward_left, forward_right, backward_left, backward_right
+
+board = Board()
+board.show()
--- Piece.py
+++ Piece.py
@@ -1,6 +1,13 @@
+from Color import Color
+
 class Piece():
     def __init__(self,x,y,color):
         self._x = x
         self._y = y
         self._color = color
 
+    def __str__(self):
+        if self._color == Color.BLACK:
+            return 'B'
+        else:
+            return 'W'
